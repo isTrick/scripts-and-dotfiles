7421a2007dc22d562f8cfb41273243e2abe6eb38
Patrick Girardi <111985260+trickgirardi@users.noreply.github.com>
2026-06-16 18:49:07 -0300

    Update fish config for greeting and environment setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,14 @@
-
+if status is-interactive
+    set -g fish_greeting "👨🏻‍💻$USER"
+end
 
 starship init fish | source
+
+eval (keychain --eval --quiet --agents ssh id_ed25519)
+fish_add_path $HOME/.local/bin
+
+if test -d /home/linuxbrew/.linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
+fnm env --use-on-cd | source
